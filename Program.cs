18880ac70041bc182f9802b03e00207882cb39de
bchaacke <113635066+bchaacke@users.noreply.github.com>
2023-04-19 16:02:33 -0600

    Created Program

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Raylib_cs;
+
+class Program {
+    static void Main () {
+        Raylib.InitWindow(306, 65, "Hello World");
+        Raylib.SetTargetFPS(1);
+        while (!Raylib.WindowShouldClose()) {
+            Raylib.BeginDrawing();
+            Raylib.DrawText("Hello World", 10, 10, 50, Color.WHITE);
+            Raylib.EndDrawing();
+        }
+    }
+}
